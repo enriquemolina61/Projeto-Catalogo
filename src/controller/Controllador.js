cf14454e85b9429f16018a86fdc8fae0152677e2
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-05-07 23:17:11 -0300

    rotas-detalhes-db

--- src/controller/Controllador.js
+++ src/controller/Controllador.js
@@ -1,3 +1,27 @@
-export const getIndex = (req, res) => {
-    res.render("index.ejs");
+import { connection } from "../database/connection.js";
+import { filmes } from "../model/filmes.js";
+import path from "path";
+
+export const getIndex = async (req, res) => {
+  try {
+    //   const listFilmes = await connection.query("SELECT * FROM filmes",{
+    //       model: filmes
+    //   });
+    const listFilmes = await filmes.findAll();
+    res.render("index.ejs", {
+      listFilmes,
+    });
+  } catch (err) {
+    res.send(error.message);
   }
+};
+
+export const getDetalhes = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const filmesDetalhes = await filmes.findByPk(id);
+    res.render("detalhes.ejs", { filmesDetalhes });
+  } catch (err) {
+    req.send(error.message);
+  }
+};
